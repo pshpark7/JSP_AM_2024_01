713f5b7baf89d41345333909c750346f5f2c758e
pshpark7 <pshpark07@gmail.com>
2024-01-19 17:14:18 +0900

    구구단, 파라미터 사용

--- src/main/java/com/KoreaIT/java/jsp_AM/PrintDanServlet.java
+++ src/main/java/com/KoreaIT/java/jsp_AM/PrintDanServlet.java
@@ -1,26 +1,40 @@
 package com.KoreaIT.java.jsp_AM;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet("/printDan")
-public class PrintDanServlet extends HttpServlet {
+public class printDanServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
 		response.setContentType("text/html;charset=UTF-8");
-		response.getWriter().append("==8단==<br>");
-		
-		int dan = 8;
-		for (int i = 1; i<=9; i++) {
-			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan* i));
+
+		String inputedDan = request.getParameter("dan");
+		String inputedLimit = request.getParameter("limit");
+
+		if (inputedDan == null) {
+			inputedDan = "1";
 		}
-			
-				
+		if (inputedLimit == null) {
+			inputedLimit = "1";
+		}
+
+
+		int dan = Integer.parseInt(inputedDan);
+		int limit = Integer.parseInt(inputedLimit);
+
+		response.getWriter().append(String.format("==%d단==<br>", dan));
+
+		for (int i = 1; i <= limit; i++) {
+			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+		}
 	}
 
 }
