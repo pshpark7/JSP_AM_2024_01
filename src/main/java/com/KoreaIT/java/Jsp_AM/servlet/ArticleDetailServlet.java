44170abd316d31a4f4b52828d4bdc62699a45f38
pshpark7 <pshpark07@gmail.com>
2024-01-22 15:23:42 +0900

    리팩토링, 메인페이지 추가

--- src/main/java/com/KoreaIT/java/Jsp_AM/servlet/ArticleDetailServlet.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/servlet/ArticleDetailServlet.java
@@ -1,21 +1,20 @@
-package com.KoreaIT.java.Jsp_AM;
+package com.KoreaIT.java.Jsp_AM.servlet;
 
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Map;
+
+import com.KoreaIT.java.Jsp_AM.util.DBUtil;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-@WebServlet("/article/list")
-public class ArticleListServlet extends HttpServlet {
-
+@WebServlet("/article/detail")
+public class ArticleDetailServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
@@ -26,27 +25,23 @@ public class ArticleListServlet extends HttpServlet {
 			System.out.println("클래스가 없습니다.");
 			e.printStackTrace();
 		}
-
 		String url = "jdbc:mysql://127.0.0.1:3306/JSP_AM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
 		String user = "root";
 		String password = "";
-
 		Connection conn = null;
 
 		try {
-			conn = DriverManager.getConnection(url, "root", "");
+			conn = DriverManager.getConnection(url, user, password);
 			response.getWriter().append("연결 성공!");
 
-			DBUtil dbUtil = new DBUtil(request, response);
+			int id = Integer.parseInt(request.getParameter("id"));
 
-			String sql = "SELECT * FROM article;";
+			String sql = String.format("SELECT * FROM article WHERE id = %d;", id);
 
-			List<Map<String, Object>> articleRows = dbUtil.selectRows(conn, sql);
+			Map<String, Object> articleRow = DBUtil.selectRow(conn, sql);
 
-//			response.getWriter().append(articleRows.toString());
-			request.setAttribute("articleRows", articleRows);
-			request.getRequestDispatcher("/jsp/article/list.jsp").forward(request, response);
-
+			request.setAttribute("articleRow", articleRow);
+			request.getRequestDispatcher("/jsp/article/detail.jsp").forward(request, response);
 		} catch (SQLException e) {
 			System.out.println("에러 : " + e);
 		} finally {
@@ -59,5 +54,4 @@ public class ArticleListServlet extends HttpServlet {
 			}
 		}
 	}
-
 }
--- src/main/java/com/KoreaIT/java/Jsp_AM/servlet/ArticleListServlet.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/servlet/ArticleListServlet.java
@@ -1,4 +1,4 @@
-package com.KoreaIT.java.Jsp_AM;
+package com.KoreaIT.java.Jsp_AM.servlet;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -7,15 +7,15 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
+import com.KoreaIT.java.Jsp_AM.util.DBUtil;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-@WebServlet("/article/detail")
-public class ArticleDetailServlet extends HttpServlet {
-
+@WebServlet("/article/list")
+public class ArticleListServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
@@ -26,28 +26,21 @@ public class ArticleDetailServlet extends HttpServlet {
 			System.out.println("클래스가 없습니다.");
 			e.printStackTrace();
 		}
-
 		String url = "jdbc:mysql://127.0.0.1:3306/JSP_AM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
 		String user = "root";
 		String password = "";
-
 		Connection conn = null;
 
 		try {
-			conn = DriverManager.getConnection(url, "root", "");
+			conn = DriverManager.getConnection(url, user, password);
 			response.getWriter().append("연결 성공!");
 
-			DBUtil dbUtil = new DBUtil(request, response);
+			String sql = "SELECT * FROM article;";
 
-			int id = Integer.parseInt(request.getParameter("id"));
+			List<Map<String, Object>> articleRows = DBUtil.selectRows(conn, sql);
 
-//			String sql = "SELECT * FROM article WHERE id = " + id + ";";
-			String sql = String.format("SELECT * FROM article WHERE id = %d;", id);
-
-			Map<String, Object> articleRow = dbUtil.selectRow(conn, sql);
-
-			request.setAttribute("articleRow", articleRow);
-			request.getRequestDispatcher("/jsp/article/detail.jsp").forward(request, response);
+			request.setAttribute("articleRows", articleRows);
+			request.getRequestDispatcher("/jsp/article/list.jsp").forward(request, response);
 
 		} catch (SQLException e) {
 			System.out.println("에러 : " + e);
@@ -61,5 +54,4 @@ public class ArticleDetailServlet extends HttpServlet {
 			}
 		}
 	}
-
 }
